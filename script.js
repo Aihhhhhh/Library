8917d6fcde3faf74b86f4e7671b85ca215600c81
Mwaniki <102294424+Aihhhhhh@users.noreply.github.com>
2023-12-14 03:29:05 -0800

    Add a function to create new objects and use DOM Manipulation to target the form input values and link them to the object constructor

--- script.js
+++ script.js
@@ -7,28 +7,47 @@ function Book(title,author,pages,read){
     this.author=author;
     this.pages=pages;
     this.read=read;
-}
+
     this.info= function(){
         let readStatus = read? 'read': 'not yet read';
         let bookInfo= `${title} by ${author} has ${pages} pages and is ${readStatus}`;
         return bookInfo
     }
-const bone = new Book("Bnture","bivinya",265,false);
-const king =new Book("king","Queen",900,true);
+}
+
+//get the whole form
+const dialogShow = document.querySelector("#enquiry");
+//get the book title
+const bookTitle = dialogShow.querySelector("#title");
+console.log(title);
+//get the book author
+const bookAuthor = dialogShow.querySelector("#author");
+console.log(author);
+//get number of pages
+const bookPages = dialogShow.querySelector("#pages");
+console.log(pages)
+//function for creating book object
+function createBook(){
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    let pages = bookPages.value;
+
+    let newBook = new Book(title,author,pages)
+    console.log(newBook);
+}
+createBook()
 //function for adding new book objects to the array
-function addBook(book){
-    myLibrary.push(book)
-    return myLibrary
+function addBook(){
+    
 }
 addBook()
 //function that loops through the array and returns each book
 function getBook(){
     for (let i =0;i< myLibrary.length;i++){
-        console.table(myLibrary[i])
+        console.log(myLibrary[i])
     }
 }
 getBook()
-const dialogShow = document.querySelector("#enquiry")
 //display form on button click
 const addButton = document.querySelector(".add-book");
 console.log(addButton);
@@ -45,21 +64,9 @@ cancel.addEventListener("click",(event)=>{
 //add a click event listener to the submit burron
 const submit = document.querySelector("#confirm")
 submit.addEventListener("click",(event)=>{
+    createBook()
     event.preventDefault()
     dialogShow.close()
 })
-//get the book title
-const titleBook = dialogShow.querySelector("#title");
-console.log(titleBook);
-//get the book author
-const authorBook = dialogShow.querySelector("#author");
-console.log(authorBook);
-//get number of pages
-const pagesBook = dialogShow.querySelector("#pages");
-console.log(pagesBook)
-//get book read
-const readBook = dialogShow.querySelector("#read");
-console.log(readBook)
-//get book not read
-const notRead = dialogShow.querySelector("#not-read");
-console.log(notRead)
+
+
